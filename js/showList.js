78fba7c699c0e7466add29fe10eb65b080f7cb5f
AleksaHrevtsova <aleksandra.hrevtsova@gmail.com>
2021-06-20 19:03:58 +0300

    showList

---
+++ js/showList.js
@@ -0,0 +1,36 @@
+;(() => {
+  const refs = {
+    lists: document.querySelectorAll('[data-show-list]'),
+    links: document.querySelectorAll('[data-toggle-list]'),
+  }
+  console.log(refs.links)
+  refs.links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      //   console.dir()
+      if (e.target.nextElementSibling.dataset.showList === '') {
+        toggleList(e.target.nextElementSibling)
+      }
+    })
+  })
+  function toggleList(list) {
+    // list.classList.toggle('is-hidden')
+    list.style.color = 'red'
+  }
+})()
+// ;(() => {
+//   const refs = {
+//     lists: document.querySelectorAll('[data-show-list]'),
+//     links: document.querySelectorAll('[data-toggle-list]'),
+//   }
+//   console.log(refs.links)
+//   refs.links.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//       if (e.target.nextElementSibling.dataset.showList === '') {
+//         toggleList(e.target.nextElementSibling)
+//       }
+//     })
+//   })
+//   function toggleList(list) {
+//     list.classList.toggle('is-hidden')
+//   }
+// })()
